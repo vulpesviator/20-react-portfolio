a1880cf2f9ebdd73fb5ea3e739e8f83eb71812de
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-08-22 16:41:58 -0500

    Added mui contact form

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,7 +4,7 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
 
-export default function Container({currentPage}) {
+export default function Container({currentPage, setCurrentPage}) {
     if (currentPage === "About") {
         return <About />;
     } 
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { TextField, Button, MenuItem } from "@mui/material";
+import Stack from "@mui/material/Stack";
+
+export default function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const validateName = (e) => {
+        console.log(e);
+        const { name, value } = e.target;
+        setName(value);
+        if (name === "name") {
+          if (value === "") {
+            setName("");
+            setErrorMessage(`Please enter a name. This field is required.`);
+          }
+        }
+        if (value.length > 0) {
+          setErrorMessage(``);
+        }
+      };
+    
+    const validateEmail = (e) => {
+        e.preventDefault();
+        console.log(e);
+        const { name, value } = e.target;
+        setEmail(value);
+        if (name === "email") {
+          if (value === "") {
+            setEmail("");
+            setErrorMessage(
+              `Please enter a valid email address. This field is required.`
+            );
+          }
+          const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2}$/;
+          console.log(!pattern.test(value));
+          if (!pattern.test(value)) {
+            setErrorMessage(`Please enter a valid email`);
+          } else {
+            setErrorMessage("");
+          }
+        }
+      };
+
+      const validateMessage = (e) => {
+        console.log(e);
+        const { name, value } = e.target;
+        setMessage(value);
+        if (name === "message") {
+          if (value === "") {
+            setMessage("");
+            setErrorMessage(`Please enter a message. This field is required.`);
+          }
+        }
+        if (value.length > 0) {
+          setErrorMessage(``);
+        }
+      };
+  
+    return (
+    <>
+      <form style={{ display: "flex", justifyContent: "center" }}>
+        <Stack spacing={2}>
+          <MenuItem>
+            <TextField
+              defaultValue={name}
+              onBlur={validateName}
+              id="outlined-basic"
+              fullWidth
+              label="Name"
+              name="name"
+              type="text"
+              variant="outlined"
+              style={{ backgroundColor: "floralwhite" }}
+            />
+          </MenuItem>
+          <MenuItem>
+            {" "}
+            <TextField
+              defaultValue={email}
+              onBlur={validateEmail}
+              id="outlined-basic"
+              fullWidth
+              label="Email Address"
+              name="email"
+              type="email"
+              variant="outlined"
+              style={{ backgroundColor: "floralwhite" }}
+            />
+          </MenuItem>
+          <MenuItem>
+            <textarea
+              defaultValue={message}
+              onBlur={validateMessage}
+              rows="15"
+              cols="40"
+              id="outlined-basic"
+              label="Message"
+              name="message"
+              type="text"
+              variant="outlined"
+              style={{ backgroundColor: "floralwhite" }}
+              placeholder="Type your message here!"
+            />
+          </MenuItem>
+          {errorMessage && (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+          <MenuItem style={{ justifyContent: "center" }}>
+            <Button
+              id="contact-button"
+              className="pop-on-hover"
+              xs={{ width: "100%" }}
+              variant="contained"
+            >
+              Submit
+            </Button>
+          </MenuItem>
+        </Stack>
+      </form>
+    </>
+  );
+}
