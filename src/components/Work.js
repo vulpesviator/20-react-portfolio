a974675e6a99f91fa3a495b72952ed82f5b70afb
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-08-23 13:55:03 -0500

    Added rest of projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,8 +1,9 @@
+
 export const works = [
     {
         title: "Zen 4 You",
         description: "Motivational Poster Generator",
-        imageUrl: "./assets/zen-4-you.jpg",
+        imageUrl: "../assets/zen-4-you.jpg",
         deployUrl: "https://project01team03.github.io/Zen4You/",
         git: "https://github.com/vulpesviator/Zen4You",
     },
@@ -12,5 +13,26 @@ export const works = [
         imageUrl: "https://user-images.githubusercontent.com/123843930/234964270-eba8ffe6-d341-4655-8d41-9b75a286e3d3.gif",
         deployUrl: "https://vulpesviator.github.io/04-code-quiz/index.html",
         git: "https://github.com/vulpesviator/04-code-quiz",
+    },
+    {
+        title: "TravelCast",
+        description: "Forecast App",
+        imageUrl: "https://user-images.githubusercontent.com/123843930/237523918-5993cdd1-5606-43a7-8857-3ce1e5224b3f.gif",
+        deployUrl: "https://vulpesviator.github.io/06-weather-dashboard/",
+        git: "https://github.com/vulpesviator/06-weather-dashboard",
+    },
+    {
+        title: "What's For Dinner?",
+        description: "IN PROGRESS",
+        imageUrl: "https://user-images.githubusercontent.com/123843930/255024085-887c7d16-3cf0-458b-9d93-7e27ee9843ce.gif",
+        deployUrl: "https://p2t3-whats-for-dinner-22c46fecc28a.herokuapp.com/",
+        git: "https://github.com/vulpesviator/whats-for-dinner",
+    },
+    {
+        title: "Tech Blog",
+        description: "Blog utilizing MVC architecture",
+        imageUrl: "https://user-images.githubusercontent.com/123843930/253325718-d5161e3a-80c1-4b78-b215-6c93b98872a6.gif",
+        deployUrl: "https://mvc14-tech-blog-231546f0eef6.herokuapp.com/",
+        git: "https://github.com/vulpesviator/14-tech-blog",
     },
 ]
